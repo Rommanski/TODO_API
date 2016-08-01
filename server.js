2c164c5ed2f4cc73db8163670375787954ea6aeb
Dmytro Bohachevskyy <dmytro.bohachevskyy@gmail.com>
2016-08-02 00:21:34 +0300

    POST /todo was added

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [{
@@ -10,6 +12,9 @@ var todos = [{
     description: 'Go to market',
     completed: true
 }];
+var nextId = 3;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send('Todo API Root');
@@ -20,7 +25,33 @@ app.get('/todos', function (req, res) {
     res.json(todos);
 });
 
-// GET /todos/:id
+// GET /todos/:
+app.get('/todos/:id', function(req, res) {
+    var todoId = parseInt(req.params.id);
+    var matchTodo;
+
+    // Iterate of todos array. Find the match
+    todos.forEach( function(todo) {
+        if (todoId === todo.id) {
+            matchTodo = todo;
+        }
+    } );
+
+    if (matchTodo) {
+        res.json(matchTodo);
+    } else {
+        res.status(404).send("Resource doesn't found");
+    }
+});
+
+// POST /todos
+app.post('/todos', function(req, res) {
+    var body = req.body;
+    body.id = nextId++;
+
+    todos.push( body );
+    res.send(body);
+});
 
 app.listen(PORT, function() {
     console.log('Exprese server started at port: ' + PORT);

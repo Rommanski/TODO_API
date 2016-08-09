8bb732adda23e91a9469a7c4a0500be5bd80f85f
Dmytro Bohachevskyy <dmytro.bohachevskyy@gmail.com>
2016-08-10 00:25:43 +0300

    Moving to postgress database

--- db.js
+++ db.js
@@ -1,11 +1,19 @@
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-    'dialect' : 'sqlite',
-    'storage' : __dirname + '/data/database.squlite'
-});
+var env = process.env.NODE_ENV || 'development';
+var sequalize;
+
+if ( 'production' === env ) {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        'dialect' : 'postgress',
+    });
+} else {
+    sequelize = new Sequelize(undefined, undefined, undefined, {
+        'dialect' : 'sqlite',
+        'storage' : __dirname + '/data/database.squlite'
+    });
+}
 
 var db = {};
-
 db.todo = sequelize.import(__dirname + '/models/todo.js');
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;

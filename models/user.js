831e00e0bc756dbb11bc6b7295a25e2985e59cef
Dmytro Bohachevskyy <dmytro.bohachevskyy@gmail.com>
2016-08-11 00:16:51 +0300

    Hash and salt password

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 var _ = require('underscore');
+var bcrypt = require('bcrypt');
 
 module.exports = function (sequelise, DataTypes) {
     return sequelize.define('user', {
@@ -10,11 +11,25 @@ module.exports = function (sequelise, DataTypes) {
                 isEmail : true
             }
         },
-        password : {
+        salt : {
             type : DataTypes.STRING,
+        },
+        password_hash : {
+            type : DataTypes.STRING,
+        },
+        password : {
+            type : DataTypes.VIRTUAL,
             allowNull : false,
             validate : {
                 len : [7, 100]
+            },
+            set : function(value) {
+                var salt = bcrypt.genSaltSync(10);
+                var hashedPassword = bcrypt.hashSync(value, salt);
+
+                this.setDataValue('password', value);
+                this.setDataValue('salt', salt);
+                this.setDataValue('password_hash', hashedPassword);
             }
         }
     }, {
@@ -24,6 +39,12 @@ module.exports = function (sequelise, DataTypes) {
                 if ( _.isString(user.email) ) {
                     user.email = user.email.toLowerCase();
                 }
+            }
+        },
+        instanceMethods : {
+            toPublicJSON : function() {
+                var json = this.toJSON();
+                return _.pick(json, 'id', 'email', 'updatedAt', 'createdAt');
             }
         }
     });

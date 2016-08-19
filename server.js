be9d58ec40b8bf48a0e81f3d3f3a915415005bf0
Dmytro Bohachevskyy <dmytro.bohachevskyy@gmail.com>
2016-08-19 14:44:07 +0300

    Force sync now true

--- server.js
+++ server.js
@@ -175,7 +175,7 @@ app.post('/users/login', function(req, res) {
     } );
 });
 
-db.sequelize.sync().then( function() {
+db.sequelize.sync({force : true}).then( function() {
     app.listen(PORT, function() {
         console.log('Exprese server started at port: ' + PORT);
     });
